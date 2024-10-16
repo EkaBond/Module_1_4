8da76612884675168abb48633b8650cae1283232
EkaBond <ekabond87@gmail.com>
2024-10-16 15:54:48 +0300

    Add files via upload

---
+++ Module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input('Введита любые два слова: ') #шаг1
+print(len(my_string))                          #шаг2
+print(my_string.upper())                       #шаг3
+print(my_string.lower())                       #шаг4
+print(my_string.replace(' ',''))   #шаг5
+print(my_string[0])                            #шаг6
+print(my_string[-1])                           #шаг7
